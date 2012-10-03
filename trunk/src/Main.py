855249cdc0441638ae196e9af0ec0a4d09916866
pie.or.paj@gmail.com <pie.or.paj@gmail.com@0f0b2a03-ea3c-d512-871a-f59f6c50c605>
2012-10-03 11:26:49 +0000

    Move to correct folder

---
+++ trunk/src/Main.py
@@ -0,0 +1,16 @@
+'''
+@author: Christoffer Ackelman
+'''
+
+class Main:
+    def __init__(self):
+        '''
+        Main loop:
+            Get input data from Input
+            Do logic
+            Send command to Engines
+        '''
+        pass
+
+if __name__ == '__main__':
+    main = Main()
