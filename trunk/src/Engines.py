91159e229949602865589b62297f1651e23f5073
pie.or.paj@gmail.com <pie.or.paj@gmail.com@0f0b2a03-ea3c-d512-871a-f59f6c50c605>
2012-10-03 11:25:52 +0000

    Move to correct folder, add stop

---
+++ trunk/src/Engines.py
@@ -0,0 +1,19 @@
+'''
+@author: Christoffer Ackelman
+'''
+
+class Engines:
+    def forward(self):
+        pass
+   
+    def backward(self):
+        pass
+   
+    def turnRight(self):
+        pass
+   
+    def turnLeft(self):
+        pass
+        
+    def stop(self):
+        pass
