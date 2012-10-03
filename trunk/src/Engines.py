7151e085646eb6632fec327d0e8aaa933dcf15d5
pie.or.paj@gmail.com <pie.or.paj@gmail.com@0f0b2a03-ea3c-d512-871a-f59f6c50c605>
2012-10-03 13:12:45 +0000

    Wrote Engine controll functions

--- trunk/src/Engines.py
+++ trunk/src/Engines.py
@@ -3,18 +3,59 @@
 @author: Rasmus Eneman
 '''
 
+import wiringpi
+
+#Constants
+leftEngineCW = 2
+leftEngineCCW = 3
+rightEngineCW = 4
+rightEngineCCW = 17
+OUTPUT = 1
+LOW = 0
+HIGH = 1
+
 class Engines:
+    def __init__(self):
+        wiringpi.wiringPiSetup()
+        wiringpi.pinMode(leftEngineCW, OUTPUT)
+        wiringpi.pinMode(leftEngineCCW, OUTPUT)
+        wiringpi.pinMode(rightEngineCW, OUTPUT)
+        wiringpi.pinMode(rightEngineCCW, OUTPUT)
+    
     def forward(self):
-        pass
+        self.leftEngineCW()
+        self.rightEngineCCW()
    
     def backward(self):
-        pass
+        self.leftEngineCCW()
+        self.rightEngineCW()
    
     def turnRight(self):
-        pass
+        self.leftEngineCCW()
+        self.rightEngineCCW()
    
     def turnLeft(self):
-        pass
+        self.leftEngineCW()
+        self.rightEngineCW()
         
     def stop(self):
-        pass
+        wiringpi.digitalWrite(leftEngineCCW, LOW)
+        wiringpi.digitalWrite(leftEngineCW, LOW)
+        wiringpi.digitalWrite(rightEngineCCW, LOW)
+        wiringpi.digitalWrite(rightEngineCW, LOW)
+    
+    def leftEngineCW(self):
+        wiringpi.digitalWrite(leftEngineCCW, LOW)
+        wiringpi.digitalWrite(leftEngineCW, HIGH)
+    
+    def leftEngineCCW(self):
+        wiringpi.digitalWrite(leftEngineCW, LOW)
+        wiringpi.digitalWrite(leftEngineCCW, HIGH)
+    
+    def rightEngineCW(self):
+        wiringpi.digitalWrite(rightEngineCCW, LOW)
+        wiringpi.digitalWrite(rightEngineCW, HIGH)
+    
+    def rightEngineCCW(self):
+        wiringpi.digitalWrite(rightEngineCW, LOW)
+        wiringpi.digitalWrite(rightEngineCCW, HIGH)
